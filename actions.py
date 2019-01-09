324a77acb97597babc7090b097d42e2a1b50601f
Deepa, Yuga <yugadeepa.c@gmail.com>
2019-01-09 18:26:18 +0530

    fixing email sender

--- actions.py
+++ actions.py
@@ -127,16 +127,17 @@ class ActionSendEmail(Action):
 	def run(self, dispatcher, tracker, domain):
 		dispatcher.utter_message("Inside email action")
 		mailid = tracker.get_slot('emailid')
+		dispatcher.utter_message("email id parsed = "+mailid)
 		smtpServer = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 		emailContent = MIMEMultipart('alternative')
-		messageBody = "<h3>restaurant search results</h3>"
+		messageBody = "<h3>restaurant search results from chatbot</h3>"
 		emailContent['Subject'] = 'Zomato search results'
 		emailContent['From'] = 'chatbotemailer123@gmail.com'
 		emailContent['To'] = 'yugadeepa.c@gmail.com'
 		emailContent.attach(MIMEText(messageBody,'html'))
-		server.ehlo()
-		server.login('chatbotemailer123@gmail.com','learn.123')
-		server.send_message(emailContent)
-		server.quit()
+		smtpServer.ehlo()
+		smtpServer.login('chatbotemailer123@gmail.com','learn.123')
+		smtpServer.send_message(emailContent)
+		smtpServer.quit()
 		dispatcher.utter_message("Sent email successfully to "+mailid)
 		return [SlotSet('emailid',mail)]
